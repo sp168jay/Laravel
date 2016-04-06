7ba61c44e98b1ba0a7186d264bdb57b2c75652c1
sp168jay <sp168jay@gmail.com>
2016-04-06 11:23:43 +0800

    練習

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Task;//使用之間建立用來對應資料表的模型
+use Illuminate\Http\Request;//用來處理連線使用者的要求
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,6 +14,34 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['middleware ' => 'web'], function(){//使用框架中間層保護相關 URL
+
+
+    //規劃路由
+    Route::get('/', function () {//顯示所有任務
+        return view('welcome');//網頁使用者介面
+
+    });
+
+        Route::post('/task', function (Request $request) {//新增任務
+        $validator = Validator::make(%request->all()),[
+            'name'=>'required|max:255';
+        ]);
+            if($validator->fails()){
+                return redirect('/')
+                    ->withInput()
+                    ->withErrors($validator);
+            }
+
+    });
+
+        Route::deltet('/task/{task}', function (Task $task) {//刪除任務
+        //
+
+    });
+
+    
+    
+
 });
